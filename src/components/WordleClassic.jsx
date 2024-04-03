9bb56cfc053fbddfec3e22a24fb901aacf85cbff
KaterinichViktor <107800265+KaterinichViktor@users.noreply.github.com>
2024-04-03 18:16:25 +0300

    x

--- src/components/WordleClassic.jsx
+++ src/components/WordleClassic.jsx
@@ -61,47 +61,93 @@ function WordleGame() {
     }
   };
 
+  // const updateCellColors = (word, correctRef) => {
+  //   const gridCopy = [...grid];
+  //   const buttonsToColor = {}; // Object to store buttons to color
+    
+  //   for (let i = 0; i < word.length; i++) {
+  //     const char = word[i];
+  //     const cell = gridCopy[currentRow][i];
+  
+  //     setTimeout(() => { // Add timeout for cell coloring
+  //       if (dailyWord[i] === char) {
+  //         cell.state = 'flipped-green'; 
+  //         correctRef.current++; 
+  //       } else if (dailyWord.includes(char)) {
+  //         cell.state = 'flipped-yellow'; 
+  //       } else {
+  //         cell.state = 'flipped-red'; 
+  //       }
+  
+  //       const buttonId = `button-${char.toLowerCase()}`;
+  //       buttonsToColor[buttonId] = cell.state; // Store button ID and corresponding state
+  
+  //       setGrid([...gridCopy]); // Update grid after coloring cell
+    
+  //       if (correctRef.current === 5) {
+  //         toast.success('Congratulations! You won!');
+  //         inputRef.current.blur();
+  //       }
+  //     }, 150 * (i + 1)); // Timeout for cell coloring
+  //   }
+  
+  //   // Color buttons after all cells are processed
+  //   setTimeout(() => {
+  //     Object.keys(buttonsToColor).forEach(buttonId => {
+  //       const state = buttonsToColor[buttonId];
+  //       colorButtonByCellState(buttonId, state);
+  //     });
+  //   }, 150 * word.length + 500); // Timeout for coloring buttons after all cells are processed
+  // };
+
   const updateCellColors = (word, correctRef) => {
     const gridCopy = [...grid];
     const buttonsToColor = {}; // Object to store buttons to color
-    
+    const coloredIndices = {}; // Object to store colored indices
+
     for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      const cell = gridCopy[currentRow][i];
-  
-      setTimeout(() => { // Add timeout for cell coloring
-        if (dailyWord[i] === char) {
-          cell.state = 'flipped-green'; 
-          correctRef.current++; 
-        } else if (dailyWord.includes(char)) {
-          cell.state = 'flipped-yellow'; 
-        } else {
-          cell.state = 'flipped-red'; 
-        }
-  
-        const buttonId = `button-${char.toLowerCase()}`;
-        buttonsToColor[buttonId] = cell.state; // Store button ID and corresponding state
-  
-        setGrid([...gridCopy]); // Update grid after coloring cell
-    
-        if (correctRef.current === 5) {
-          toast.success('Congratulations! You won!');
-          inputRef.current.blur();
-        }
-      }, 150 * (i + 1)); // Timeout for cell coloring
+        const char = word[i];
+        const cell = gridCopy[currentRow][i];
+
+        setTimeout(() => { // Add timeout for cell coloring
+            if (dailyWord[i] === char) {
+                cell.state = 'flipped-green'; 
+                correctRef.current++; 
+                
+            } else if (dailyWord.includes(char)) {
+                // Find the first occurrence of the character in the secret word
+                const firstOccurrenceIndex = dailyWord.indexOf(char);
+                if (coloredIndices[char] === undefined) {
+                    // If the letter hasn't been colored before, color it yellow
+                    cell.state = 'flipped-yellow';
+                    coloredIndices[char] = true;
+                }else if(coloredIndices[char] === true) {
+                  cell.state = 'flipped-red';
+                }
+            } else {
+                cell.state = 'flipped-red'; 
+            }
+
+            const buttonId = `button-${char.toLowerCase()}`;
+            buttonsToColor[buttonId] = cell.state; // Store button ID and corresponding state
+
+            setGrid([...gridCopy]); // Update grid after coloring cell
+
+            if (correctRef.current === 5) {
+                toast.success('Congratulations! You won!');
+                inputRef.current.blur();
+            }
+        }, 150 * (i + 1)); // Timeout for cell coloring
     }
-  
+
     // Color buttons after all cells are processed
     setTimeout(() => {
-      Object.keys(buttonsToColor).forEach(buttonId => {
-        const state = buttonsToColor[buttonId];
-        colorButtonByCellState(buttonId, state);
-      });
+        Object.keys(buttonsToColor).forEach(buttonId => {
+            const state = buttonsToColor[buttonId];
+            colorButtonByCellState(buttonId, state);
+        });
     }, 150 * word.length + 500); // Timeout for coloring buttons after all cells are processed
-  };
-  
-  
-  
+};
   
   const updateCells = (value) => {
     const gridCopy = [...grid];
