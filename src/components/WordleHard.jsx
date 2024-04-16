7f649b6bf3f9a6510655e1529c4437094ddc829a
KaterinichViktor <107800265+KaterinichViktor@users.noreply.github.com>
2024-04-16 14:57:44 +0300

    x

--- src/components/WordleHard.jsx
+++ src/components/WordleHard.jsx
@@ -1,84 +1,227 @@
-// import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import dictArray from "./dictionary";
+import { TopWordle } from './TopPanel';
 
-// function GuessCode() {
-//   const [currentRow, setCurrentRow] = useState(0);
-//   const [hiddenInput, setHiddenInput] = useState('');
-//   const [grid, setGrid] = useState([Array.from({ length: 4 }, () => '')]);
-//   const [correctAmnt, setCorrectAmnt] = useState(0);
+import {Keyboard, colorButtonByCellState} from './keyboard'; // Import the Keyboard component
 
-//   const inputRef = useRef(null); // Create a ref for the input element
+// import { getRandomWord } from "./dictionary";
 
-//   function generateRandomNumber() {
-//         return Math.floor(Math.random() * 9000) + 1000;
-//     }
+// const dailyWord = getRandomWord().toUpperCase();
+const dailyWord = "Apple".toUpperCase();
+// let correct = 0;
+
+
+function WordleGame() {
+  const [currentRow, setCurrentRow] = useState(0);
+  const [hiddenInput, setHiddenInput] = useState('');
+  const [grid, setGrid] = useState(Array.from({ length: 6 }, () => Array.from({ length: 5 }, () => ({ content: '', state: '' }))));
+
+  const correctRef = useRef(0);
+
+  useEffect(() => {
+    correctRef.current = 0; // Reset correctRef to 0 when a new game starts
+  }, [currentRow]); // Reset correctRef whenever currentRow changes
+  
+
+  const inputRef = useRef(null);
+
+  const handleInputChange = (e) => {
+    setHiddenInput(e.target.value.toUpperCase());
+    updateCells(e.target.value.toUpperCase());
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const wordExists = await checkWordInDictionary(hiddenInput);
+    if (!wordExists) {
+      toast.error('Not a valid word'); 
+      return;
+    }
+    updateCellColors(hiddenInput, correctRef);
+    switchToNextRow();
+    clearInput();
     
-//   let random4 = generateRandomNumber();
+    // Unfocus the input field when the game is won
+    // if (correct === 5) {
+    //   toast.success('Congratulations! You won!');
+    //   inputRef.current.blur();
+    // }
+  };
+  
 
-//   const handleInputChange = (e) => {
-//     setHiddenInput(e.target.value.replace(/\D/g, '').slice(0, 4));
-//     updateCells(e.target.value);
-//   };
+  const checkWordInDictionary = async (word) => {
+    try {
+      const lowerCaseWord = word.toLowerCase();
+      return dictArray.some(dictWord => dictWord.toLowerCase() === lowerCaseWord);
+    } catch (error) {
+      console.error('Error while checking word in dictionary:', error);
+      return false;
+    }
+  };
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (hiddenInput.length !== 4) {
-//       setMessage('Please enter a 4-digit number');
-//       return;
-//     }
-
-
-//     switchToNextRow();
-//     setCorrectAmnt(getCorrectAmount(hiddenInput));
-//     clearInput();
-//   };
-
-//   const getCorrectAmount = (number) => {
-//     let correctAmnt = 0;
-//     for (let i = 0; i < 4; i++) {
-//       if (random4[i] === number[i]) {
-//         correctAmnt++;
-//       }
-//     }
-//     return correctAmnt;
-//   };
-
-//   const updateCells = (value) => {
+//   const updateCellColors = (word, correctRef) => {
 //     const gridCopy = [...grid];
-//     for (let i = 0; i < gridCopy[currentRow].length; i++) {
-//       if (i < value.length) {
-//         gridCopy[currentRow][i].content = value[i];
-//       } else {
-//         gridCopy[currentRow][i].content = '';
-//       }
+//     const buttonsToColor = {}; // Object to store buttons to color
+    
+//     for (let i = 0; i < word.length; i++) {
+//       const char = word[i];
+//       const cell = gridCopy[currentRow][i];
+  
+//       setTimeout(() => { // Add timeout for cell coloring
+//         if (dailyWord[i] === char) {
+//           cell.state = 'flipped-green'; 
+//           correctRef.current++; 
+//         } else if (dailyWord.includes(char)) {
+//           cell.state = 'flipped-yellow'; 
+//         } else {
+//           cell.state = 'flipped-red'; 
+//         }
+  
+//         const buttonId = `button-${char.toLowerCase()}`;
+//         buttonsToColor[buttonId] = cell.state; // Store button ID and corresponding state
+  
+//         setGrid([...gridCopy]); // Update grid after coloring cell
+    
+//         if (correctRef.current === 5) {
+//           toast.success('Congratulations! You won!');
+//           inputRef.current.blur();
+//         }
+//       }, 150 * (i + 1)); // Timeout for cell coloring
 //     }
-//     setGrid(gridCopy);
+  
+//     // Color buttons after all cells are processed
+//     setTimeout(() => {
+//       Object.keys(buttonsToColor).forEach(buttonId => {
+//         const state = buttonsToColor[buttonId];
+//         colorButtonByCellState(buttonId, state);
+//       });
+//     }, 150 * word.length + 500); // Timeout for coloring buttons after all cells are processed
 //   };
 
+  const updateCellColors = (word, correctRef) => {
+    const gridCopy = [...grid];
+    const buttonsToColor = {}; // Object to store buttons to color
+    const coloredIndices = {}; // Object to store colored indices
 
-//   const switchToNextRow = () => {
-//     setCurrentRow(currentRow + 1);
-//   };
+    for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        const cell = gridCopy[currentRow][i];
 
-//   const clearInput = () => {
-//     setHiddenInput('');
-//   };
+        setTimeout(() => { // Add timeout for cell coloring
+            if (dailyWord[i] === char) {
+                cell.state = 'flipped-green'; 
+                correctRef.current++; 
+                
+            } else if (dailyWord.includes(char)) {
+                // Find the first occurrence of the character in the secret word
+                // const firstOccurrenceIndex = dailyWord.indexOf(char);
+                if (coloredIndices[char] === undefined) {
+                    // If the letter hasn't been colored before, color it yellow
+                    cell.state = 'flipped-yellow';
+                    coloredIndices[char] = true;
+                }else if(coloredIndices[char] === true) {
+                  cell.state = 'flipped-red';
+                }
+            } else {
+                cell.state = 'flipped-red'; 
+            }
 
-//   return (
-//     <div id="game-container">
-//       <form onSubmit={handleSubmit}>
-//         <input className='hdn-input' type="text" value={hiddenInput} onChange={handleInputChange} maxLength="4" autoFocus ref={inputRef}  />
-//       </form>
+            const buttonId = `button-${char.toLowerCase()}`;
+            buttonsToColor[buttonId] = cell.state; // Store button ID and corresponding state
 
-//       <div className="row">
-//         {grid[currentRow].map(({ content, color }, cellIndex) => (
-//           <div key={cellIndex} className={`cell ${color}`}>
-//             {content}
-//           </div>
-//         ))}
-//         <div className="correct-count">{correctAmnt}</div>
-//       </div>
-//     </div>
-//   );
-// }
+            setGrid([...gridCopy]); // Update grid after coloring cell
 
-// export default GuessCode;
+            if (correctRef.current === 5) {
+                toast.success('Congratulations! You won!');
+                inputRef.current.blur();
+            }
+        }, 150 * (i + 1)); // Timeout for cell coloring
+    }
+
+    // Color buttons after all cells are processed
+    setTimeout(() => {
+        Object.keys(buttonsToColor).forEach(buttonId => {
+            const state = buttonsToColor[buttonId];
+            colorButtonByCellState(buttonId, state);
+        });
+    }, 150 * word.length + 500); // Timeout for coloring buttons after all cells are processed
+};
+  
+  const updateCells = (value) => {
+    const gridCopy = [...grid];
+    for (let i = 0; i < gridCopy[currentRow].length; i++) {
+      if (i < value.length) {
+        gridCopy[currentRow][i].content = value[i];
+        gridCopy[currentRow][i].state = 'active';
+      } else {
+        gridCopy[currentRow][i].content = '';
+        gridCopy[currentRow][i].state = '';
+      }
+    }
+    setGrid(gridCopy);
+  };
+  
+
+  const switchToNextRow = () => {
+    if (currentRow < 5) {
+      setCurrentRow(currentRow + 1);
+    }
+  };
+
+  const clearInput = () => {
+    setHiddenInput('');
+  };
+  const handleInputBlur = () => {
+    if (correctRef.current !==5) {
+
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <div className='wordle-box'>
+      <TopWordle/>
+      <div id="game-container">
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={true} 
+          closeButton={false} 
+        />
+        <form onSubmit={handleSubmit}>
+          <input className='hdn-input-wordle' type="text" value={hiddenInput} onChange={handleInputChange} maxLength="5" autoFocus onBlur={handleInputBlur} ref={inputRef}  />
+        </form>
+      
+        {grid.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map(({ content, state }, cellIndex) => (
+                <div key={cellIndex} className={`cell-wordle ${state}`}>
+                  <div className="cell-wordle-inner">
+                    <div className="cell-wordle-front">
+                      <div className="cell-wordle-content">{content}</div>
+                    </div>
+                    <div className="cell-wordle-back">
+                      <div className="cell-wordle-content">{content}</div>
+                    </div>
+                  </div>
+                </div>
+            ))}
+          </div>
+        ))}
+
+        <Keyboard
+          handleSubmit={handleSubmit}
+          setHiddenInput={setHiddenInput}
+          hiddenInput={hiddenInput}
+          updateCells={updateCells}
+        />
+
+      </div>
+    </div>
+
+  );
+}
+
+export default WordleGame;
